15c24551f82178b85ee031e733dd8cd7a9b865e2
unknown <10639273+RobiFox@users.noreply.github.com>
2022-06-28 16:47:41 +0200

    enemy component support

--- RoombaMod/EnemySpawner.cs
+++ RoombaMod/EnemySpawner.cs
@@ -16,13 +16,27 @@ namespace Thicket
             Filth, Stray, Schism, Soldier, BigMinos, Stalker, Sisyphus, Swordsmachine, Drone, Streetcleaner, V2, Mindflayer, MaliciousFace, Cerberus, HideousMass, Gabriel, Virtue, SomethingWicked, FleshPrison, MinosPrime
         }
         
-        void Start() 
-        {
+        void Start() {
             var joe = GameObject.Instantiate(SceneConstructor.bestiary[(int)enemyVar].gameObject);
             joe.transform.parent = transform.parent;
             joe.transform.localPosition = transform.localPosition;
             joe.transform.localRotation = transform.localRotation;
+            
+            // get list of all components
+            var components = GetComponents<Component>();
+            
+            foreach(Component component in components) {
+                if (component is EnemySpawner) continue;
+                Component c;
+                if(!joe.TryGetComponent(component.GetType(), out c)) {
+                    joe.AddComponent(component.GetType());
+                }
+                // set all fields to match
+                var fields = component.GetType().GetFields();
+                foreach(var field in fields) {
+                    field.SetValue(c, field.GetValue(component));
+                }
+            }
         }
-
     }
 }
